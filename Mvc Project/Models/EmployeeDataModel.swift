97767db7bd5a53bebf1427b8327426dd2d48ac84
ravibishnoi <rbk4997@gmail.com>
2020-04-14 14:01:00 +0530

    first commit

---
+++ Mvc Project/Models/EmployeeDataModel.swift
@@ -0,0 +1,9 @@
+//
+//  EmployeeDataModel.swift
+//  Mvc Project
+//
+//  Created by AshutoshD on 09/04/20.
+//  Copyright © 2020 ravindraB. All rights reserved.
+//
+
+import Foundation
---
+++ Mvc Project/Models/EmployeeDataModelItem.swift
@@ -0,0 +1,9 @@
+//
+//  EmployeeDataModelItem.swift
+//  Mvc Project
+//
+//  Created by AshutoshD on 09/04/20.
+//  Copyright © 2020 ravindraB. All rights reserved.
+//
+
+import Foundation
